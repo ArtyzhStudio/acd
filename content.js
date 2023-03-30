b08dae305d2b18ab131c4439d3eba82a5c121051
Artyzh <artyzh@inbox.ru>
2023-03-30 23:22:41 +0300

    Cleared git file tree

--- content.js
+++
@@ -1,3 +0,0 @@
-let projSec_extension_respose = confirm("Please check URL link you are trying to connect to. I suppose it's wrong. Do you want stay on it?");
-
-chrome.runtime.sendMessage({ "projSec_extension_respose": projSec_extension_respose });
